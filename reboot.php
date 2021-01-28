f829f4fd3dceb3b90fcff1eeb23a561752ebb3db
Theodis Butler <theodis@megalithtechnologies.com>
2021-01-28 06:15:40 +0000

    Create reboot.php
    Reboot Obihai

---
+++ reboot.php
@@ -0,0 +1,45 @@
+<?php
+error_reporting(E_ALL); 
+ini_set( 'display_errors','1');
+
+$host = '192.168.1.1';   
+//$pagename = 'callstatus.htm';
+$pagename = 'rebootgetconfig.htm';
+$scheme = 'http';
+$url = "{$scheme}://{$host}/{$pagename}";
+$username = "admin";
+$password = "admin";
+$options = array(
+        CURLOPT_URL            => $url,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_IGNORE_CONTENT_LENGTH=> true,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_SSL_VERIFYPEER => false,    // for https
+        CURLOPT_USERPWD        => $username . ":" . $password,
+        CURLOPT_HTTPAUTH       => CURLAUTH_DIGEST
+);
+
+$ch = curl_init();
+
+curl_setopt_array( $ch, $options );
+
+try {
+  $raw_response  = curl_exec( $ch );
+
+  // validate CURL status
+  if(curl_errno($ch))
+     throw new Exception(curl_error($ch), 500);
+  // validate HTTP status code (user/password credential issues)
+  $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  if ($status_code != 200)
+      throw new Exception("Response with Status Code [" . $status_code . "].", 500);
+}
+
+ catch(Exception $ex) {
+    if ($ch != null) curl_close($ch);
+     throw new Exception($ex);
+}
+
+echo ($raw_response);
+
