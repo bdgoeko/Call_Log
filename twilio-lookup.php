2c45b5e4b88f396bef812fb761d125ca073b0531
Theodis Butler <theodis@megalithtechnologies.com>
2021-01-28 04:27:30 +0000

    Create twilio-lookup.php
    For Twilio Lookup integration (costly, should be last resort after other verifications)

---
+++ twilio-lookup.php
@@ -0,0 +1,18 @@
+<?php
+
+// Update the path below to your autoload.php,
+// see https://getcomposer.org/doc/01-basic-usage.md
+require_once '/path/to/vendor/autoload.php';
+
+use Twilio\Rest\Client;
+
+// Find your Account Sid and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+$sid = getenv("TWILIO_ACCOUNT_SID");
+$token = getenv("TWILIO_AUTH_TOKEN");
+$twilio = new Client($sid, $token);
+
+$phone_number = $twilio->lookups->v1->phoneNumbers("+15108675310")
+                                    ->fetch(["type" => ["carrier"]]);
+
+print($phone_number->carrier);
